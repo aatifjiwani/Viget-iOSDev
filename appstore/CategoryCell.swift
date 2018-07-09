80313f079437e44b658a24e428a180f4c8dccfe2
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-09 14:33:28 -0600

    Added images to each cell

--- appstore/CategoryCell.swift
+++ appstore/CategoryCell.swift
@@ -70,12 +70,17 @@ class AppCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-   /* let imageView = UIImageView = {
+    let imageView: UIImageView = {
         let iv = UIImageView()
-        iv.image = UIImage(named: <#T##String#>)
-    }()_*/
+        iv.image = UIImage(named: "frozen")
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 16
+        return iv
+    }()
     
     func setupViews() {
-        backgroundColor = UIColor.black
+        addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+        
     }
 }
