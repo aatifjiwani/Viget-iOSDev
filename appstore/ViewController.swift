ede169cd2fd7dccc9b17acdbfccef478c277cdd0
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-09 11:13:54 -0600

    Added initial collection of 3 boxes named category

--- appstore/ViewController.swift
+++ appstore/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeaturedAppsController: UICollectionViewController {
 
+    private let cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellID)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! CategoryCell
+        return cell
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
 
 }
 
+class CategoryCell : UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.red
+    }
+    
+}
