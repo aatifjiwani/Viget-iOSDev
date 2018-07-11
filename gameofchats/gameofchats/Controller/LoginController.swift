5bb9944d579589e2e373020939da10cfd25aaeea
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-11 11:22:56 -0600

    Finished Login controller

--- gameofchats/gameofchats/Controller/LoginController.swift
+++ gameofchats/gameofchats/Controller/LoginController.swift
@@ -10,27 +10,146 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let registerButton: UIButton = {
+        let view = UIButton(type: .system)
+        view.backgroundColor = UIColor(r: 20, g: 150, b: 187, a: 1)
+        view.setTitle("Register", for: .normal)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setTitleColor(UIColor.white, for: .normal)
+        view.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let nameField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Name"
+        field.translatesAutoresizingMaskIntoConstraints = false
+        
+        return field
+    }()
+    
+    let nameSeparator: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 220, a: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let emailField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Email"
+        field.translatesAutoresizingMaskIntoConstraints = false
+        
+        return field
+    }()
+    
+    let emailSeparator: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 220, a: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let passwordField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Password"
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    let profImage: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(named: "login_image")
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFill
+        return img
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        view.backgroundColor = UIColor(r: 9, g: 47, b: 64, a: 1)
+        
+        view.addSubview(inputsContainerView)
+        view.addSubview(registerButton)
+        view.addSubview(profImage)
+        setupInputsContainerView()
+        setupRegisterButton()
+        setupProfImage()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupProfImage() {
+        profImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profImage.bottomAnchor.constraint(equalTo: inputsContainerView.topAnchor, constant: -12).isActive = true
+        profImage.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        profImage.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupRegisterButton() {
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        registerButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        registerButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        registerButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
-    */
+    
+    func setupInputsContainerView() {
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        inputsContainerView.addSubview(nameField)
+        nameField.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor, constant: 12).isActive = true
+        nameField.topAnchor.constraint(equalTo: inputsContainerView.topAnchor).isActive = true
+        nameField.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        nameField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        inputsContainerView.addSubview(nameSeparator)
+        nameSeparator.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor).isActive = true
+        nameSeparator.topAnchor.constraint(equalTo: nameField.bottomAnchor).isActive = true
+        nameSeparator.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        nameSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        inputsContainerView.addSubview(emailField)
+        emailField.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor, constant: 12).isActive = true
+        emailField.topAnchor.constraint(equalTo: nameField.bottomAnchor).isActive = true
+        emailField.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        emailField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        inputsContainerView.addSubview(emailSeparator)
+        emailSeparator.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor).isActive = true
+        emailSeparator.topAnchor.constraint(equalTo: emailField.bottomAnchor).isActive = true
+        emailSeparator.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        emailSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        inputsContainerView.addSubview(passwordField)
+        passwordField.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor, constant: 12).isActive = true
+        passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor).isActive = true
+        passwordField.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        passwordField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: 1/3).isActive = true
+    }
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+}
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
+    }
 }
