6f61ed39eddfc1bdc8df845b5df07ccfecd154dd
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-11 10:19:52 -0600

    Created login controller

---
+++ gameofchats/gameofchats/Controller/LoginController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginController.swift
+//  gameofchats
+//
+//  Created by Aatif Jiwani on 7/11/18.
+//  Copyright © 2018 Aatif Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ gameofchats/gameofchats/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gameofchats
+//
+//  Created by Aatif Jiwani on 7/11/18.
+//  Copyright © 2018 Aatif Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+    
+    @objc func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+
+}
+
