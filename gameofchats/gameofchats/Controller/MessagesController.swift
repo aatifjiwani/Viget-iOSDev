5bbc92c9357b615964c3a10bcef7714f07c2de31
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-11 15:56:58 -0600

    Finished User model and NewMessageController

--- gameofchats/gameofchats/Controller/MessagesController.swift
+++ gameofchats/gameofchats/Controller/MessagesController.swift
@@ -13,16 +13,31 @@ class MessagesController: UITableViewController {
 
     override func viewDidLoad() {
         //let ref = Database.database().reference(fromURL: "https://viget-chat.firebaseio.com/")
-        
+        checkIfUserLoggedIn()
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "new_message_icon"), style: .plain, target: self, action: #selector(handleNewMessage))
+    }
+    
+    @objc func handleNewMessage() {
+        let newMessageController = NewMessageController()
+        present(UINavigationController(rootViewController: newMessageController), animated: true, completion: nil)
     }
     
     func checkIfUserLoggedIn() {
         if Auth.auth().currentUser?.uid == nil {
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        } else {
+            let uid = Auth.auth().currentUser?.uid
+            Database.database().reference().child("users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                if let dict = snapshot.value as? [String:Any] {
+                    self.navigationItem.title = dict["name"] as? String
+                    print(snapshot)
+                }
+            }, withCancel: nil)
+            
         }
     }
     
---
+++ gameofchats/gameofchats/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  gameofchats
+//
+//  Created by Aatif Jiwani on 7/11/18.
+//  Copyright © 2018 Aatif Jiwani. All rights reserved.
+//
+
+import Foundation
+
+class User: NSObject {
+    @objc var name: String?
+    @objc var email: String?
+}
