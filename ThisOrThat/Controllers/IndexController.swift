db365e750ef827be9e9c0b2bd9b352434dcae186
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-19 12:35:20 -0600

    Added launch screen

---
+++ ThisOrThat/Controllers/IndexController.swift
@@ -0,0 +1,17 @@
+//
+//  IndexController.swift
+//  ThisOrThat
+//
+//  Created by Aatif Jiwani on 7/19/18.
+//  Copyright © 2018 Aatif Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class IndexController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+}
+
