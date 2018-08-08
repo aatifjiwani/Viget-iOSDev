c0b1e8fbeb5c52cc3b4742b0cfb1cc9e6eb61fbc
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-08-08 15:57:01 -0600

    Created comment factory

---
+++ ThisOrThatTests/Factories/CommentFactory.swift
@@ -0,0 +1,26 @@
+//
+//  CommentFactory.swift
+//  ThisOrThatTests
+//
+//  Created by Aatif Jiwani on 8/8/18.
+//  Copyright © 2018 Aatif Jiwani. All rights reserved.
+//
+
+import XCTest
+@testable import ThisOrThat
+
+enum CommentFactory {
+    static var defaultPollJSON: [String:Any] = [
+        "id": 0,
+        "user_id": 0,
+        "poll_id": 0,
+        "created_at": "2018-08-03T17:49:13.529Z",
+        "updated_at": "2018-08-03T17:49:13.529Z",
+    ]
+    
+    static func createCommentJSON(body: String = "Example Comment", username: String = "testuser") -> [String: Any] {
+        return defaultPollJSON.merging(["body": body, "username": username], uniquingKeysWith: { (_, new) -> Any in
+            new
+        })
+    }
+}
